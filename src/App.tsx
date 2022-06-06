826b013c1557f8de78406631a2e01ef27de804ea
e1ing <elvira.kisling@gmail.com>
2022-06-06 21:14:20 +0700

    App changed

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { ChangeEvent, useState, useEffect } from 'react';
 import './App.css';
+import
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  function App() {
+
+    const [message, setMessage] = useState("");
+    const [users, setUsers] = useState([{ id: 1, name: "Dima", photo: "https://via.placeholder.com/50", message: "yoyoyo" }]);
+
+    useEffect(() => {
+const ws = new WebSocket("wss://social-network.samuraijs.com/handlers/ChatHandler.ashx");
+    });
+
+    let onMessageChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+      setMessage(e.currentTarget.value)
+    }
+
+    let sendMessage = () => {
+      alert('hey!!!')
+    }
+
+    return (
+      <div className="App">
+
+        <div className="chat">
+          <div className="messages"></div>
+          {users.map(u =>
+            <div className="message">
+              <img src={u.photo} />
+              <b>{u.name}</b>
+              <span>{u.message}</span>
+            </div>
+          )}
+
+
+        </div>
+        <div className="footer">
+          <textarea onChange={onMessageChange}>{message}</textarea>
+          <button onClick={sendMessage}> Send </button>
+        </div>
+
+      </div>
+    );
+  }
 
 export default App;

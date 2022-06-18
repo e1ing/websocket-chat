6e727935338ef80217fb39fa67da8b3667928386
e1ing <elvira.kisling@gmail.com>
2022-06-18 22:07:03 +0700

    chat with scrolling made

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,67 @@
-import React, { ChangeEvent, useState, useEffect } from 'react';
+import React, { ChangeEvent, useState, useEffect, useRef } from 'react';
 import './App.css';
-import
 
-  function App() {
 
-    const [message, setMessage] = useState("");
-    const [users, setUsers] = useState([{ id: 1, name: "Dima", photo: "https://via.placeholder.com/50", message: "yoyoyo" }]);
+type MessageType = {
+  message: string,
+  photo: string,
+  userId: number,
+  userName: string
+}
 
-    useEffect(() => {
-const ws = new WebSocket("wss://social-network.samuraijs.com/handlers/ChatHandler.ashx");
-    });
+function App() {
 
-    let onMessageChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
-      setMessage(e.currentTarget.value)
-    }
+  let messagesBlockRef = useRef(null);
 
-    let sendMessage = () => {
-      alert('hey!!!')
-    }
+  const [messageText, setMessageText] = useState("");
+  const [ws, setWS] = useState<null | WebSocket>(null);
+  const [users, setUsers] = useState<MessageType[]>([]);
 
-    return (
-      <div className="App">
+  if (ws) {
+    ws.onmessage = (messageEvent: MessageEvent) => {
+      let messages = JSON.parse(messageEvent.data)
+      setUsers([...users, ...messages]);
+      //@ts-ignore
+      messagesBlockRef.current.scrollTo(0, messagesBlockRef.current.scrollHeight);
+    };
+  }
 
-        <div className="chat">
-          <div className="messages"></div>
-          {users.map(u =>
-            <div className="message">
+  useEffect(() => {
+    let localWS = new WebSocket("wss://social-network.samuraijs.com/handlers/ChatHandler.ashx");
+    setWS(localWS)
+  }, []);
+
+  let onMessageChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setMessageText(e.currentTarget.value)
+  };
+
+  let sendMessage = () => {
+    ws && ws.send(messageText)
+    setMessageText("");
+  };
+
+  return (
+    <div className="App">
+
+      <div className="chat">
+        <div className="messages" ref={messagesBlockRef}>
+          {users.map((u, index) =>
+            <div className="message" key={index}>
               <img src={u.photo} />
-              <b>{u.name}</b>
+              <b>{u.userName}</b>
               <span>{u.message}</span>
             </div>
           )}
-
-
         </div>
-        <div className="footer">
-          <textarea onChange={onMessageChange}>{message}</textarea>
-          <button onClick={sendMessage}> Send </button>
-        </div>
-
       </div>
-    );
-  }
+
+      <div className="footer">
+        <textarea onChange={onMessageChange} value={messageText}/>
+        <button onClick={sendMessage}> Send </button>
+      </div>
+
+    </div>
+  );
+}
 
 export default App;
